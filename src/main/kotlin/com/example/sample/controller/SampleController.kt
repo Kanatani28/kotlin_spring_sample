996671f35a65701772c2824a5806d7c1d3454c34
masahiro_kanatani <masahiro_kanatani@kronos-jp.net>
2019-05-04 15:10:53 +0900

    Fix Controller to use Kotlin syntax .

--- src/main/kotlin/com/example/sample/controller/SampleController.kt
+++ src/main/kotlin/com/example/sample/controller/SampleController.kt
@@ -1,21 +1,20 @@
 package com.example.sample.controller
 
 import com.example.sample.service.SampleService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
-@Controller("/")
-class SampleController {
+@Controller
+class SampleController(private val service: SampleService) {
 
-    @Autowired
-    lateinit var service: SampleService
-
-    @GetMapping("index")
+    @GetMapping("/")
     fun index(model: Model): String  {
-        val modelList = service.find()
-        model.addAttribute("modelList", modelList)
+        // model.addAttribute("modelList", service.find())
+        // model.set("modelList", service.find())
+        // Same as above .
+        model["modelList"] = service.find()
         return "index"
     }
 }
